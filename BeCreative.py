0830299a124f4bb51f523e6c68b0d8bdf7707cda
SamueIC <30849842+SamueIC@users.noreply.github.com>
2017-08-24 10:13:30 +1200

    Add files via upload

---
+++ BeCreative.py
@@ -0,0 +1,18 @@
+class Thing:
+
+    def get_row_by_value(self, array_items):
+        normal = 0
+        overweight = 0
+        obesity = 0
+        underweight = 0
+        for items in array_items:
+            if entry.bmi = 'Normal':
+                normal += 1
+            elif entry.bmi = 'Overweight':
+                fat += 1
+            elif entry.bmi = 'Obesity':
+                obesity += 1
+            elif entry.bmi = 'Underweight':
+                underweight += 1
+
+
